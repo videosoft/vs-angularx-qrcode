930eac900f5cc5100657254e2e23fdad59630714
jocinei Dalla Costa <jocinei.costa@hotmail.com>
2025-09-04 13:31:37 -0300

    fix: adjust test script

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|mjs|js)$': 'ts-jest',
